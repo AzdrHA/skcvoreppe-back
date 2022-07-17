e9c817d2afe9dd91c717854d05b0e646db6b0599
Azdra <baptiste.brand2002@gmail.com>
2022-07-17 17:37:51 +0200

    Modify getNested type

--- app/src/Util/UtilObject.ts
+++ app/src/Util/UtilObject.ts
@@ -1,16 +1,14 @@
-import { User } from '@Entity/User/User';
-
 export abstract class UtilObject {
   /**
    * @param object
    * @param keys
    */
-  public static getNested = <T>(object: T, keys: Array<keyof User>) => {
+  public static getNested = <T>(object: T, keys: Array<keyof T>) => {
     const res = {};
     keys.map((key) => {
-      return object[key] !== 'undefined'
+      return object[key]
         ? Object.assign(res, { [key]: object[key] })
-        : { [key]: undefined };
+        : { [key]: null };
     });
     return res;
   };
